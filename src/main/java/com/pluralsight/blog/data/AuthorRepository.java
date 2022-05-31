488c55a37c65901fc0084366f6b69317f76fab91
im-Ram-Raj <ramchangattu@gmail.com>
2022-05-31 12:58:48 +0530

    Completed module

--- src/main/java/com/pluralsight/blog/data/AuthorRepository.java
+++ src/main/java/com/pluralsight/blog/data/AuthorRepository.java
@@ -1,9 +1,6 @@
 package com.pluralsight.blog.data;
 
 import com.pluralsight.blog.model.Author;
-import com.pluralsight.blog.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
