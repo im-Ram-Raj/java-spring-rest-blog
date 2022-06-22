fec5e436cb2b9b5dda456e8ec3f0765775fc5900
im-Ram-Raj <ramchangattu@gmail.com>
2022-06-22 13:17:11 +0530

    Completed module

--- src/main/java/com/pluralsight/blog/data/AuthorRepository.java
+++ src/main/java/com/pluralsight/blog/data/AuthorRepository.java
@@ -2,15 +2,11 @@ package com.pluralsight.blog.data;
 
 import com.pluralsight.blog.model.Author;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Component;
 
-@Component
 @RepositoryRestResource(exported = false)
-public interface AuthorRepository extends JpaRepository<Author, Long>{
-	
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
 //	public List<Author> findByLastname(String lastname);
 }
